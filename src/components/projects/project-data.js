ab29772e0414dc103fb5dedef99b0115f6351df2
Rahul Nikam <codewithrahulnikam@gmail.com>
2022-11-06 22:34:13 +0530

    UPDATED: new project added

--- src/components/projects/project-data.js
+++ src/components/projects/project-data.js
@@ -1,19 +1,75 @@
-
 import healthAuraImage from "../../public/img/Health-Aura-project.png";
 import vaccinesNearMeImage from "../../public/img/Vaccines-Near-Me-project.png";
 import promptEngineersImage from "../../public/img/Prompt-Engineers-project.png";
+import healthassist from "../../public/img/healthassist.jpg";
 
 export const projectData = [
-    {
-      key: 1,
-      projectName: `Health Aura - Complete Healthcare System`,
-      projectType: `Full stack`,
-      projectStatus: 'Still Working',
-      githubRepo: 'https://github.com/rahulnikam2002/Health-Aura---Your-Health-With-Us',
-      projectImage: healthAuraImage,
-      projectLiveLink: '',
-      shortDescription: `Users can find nearest Hospitals, Medicals, Doctors, Technologies used in this project are NodeJS, Express, MySQL...`,
-      projectDescription: `
+  {
+    key: 1,
+    projectName: `Health Assist - Full stack native application`,
+    projectType: `Full stack`,
+    projectStatus: "Still Working",
+    githubRepo:
+      "https://github.com/rahulnikam2002/HealthAssist",
+    projectImage: healthassist,
+    projectLiveLink: "",
+    shortDescription: `smart way to manage and book appointments for Clinics, there is no need to manually verify appointment by the staff...`,
+    projectDescription: `<h2>What exactly HealthAssist is?</h2>
+
+    <p>HealthAssist is the smart way to manage and book appointments for Clinics, Users can book appointments, and there is no need to manually verify each and every appointment by the staff. Appointments will be verified automatically by the system itself.<br />
+    <br />
+    We are trying to add some unique features to this app, <strong>Join Family Account</strong>, where user can add their family members while booking an appointment and they can directly select the member who needs the treatment.<br />
+    &nbsp;</p>
+    
+    <h2>Technologies used:</h2>
+    
+    <ol>
+      <li>React Native - Main Application</li>
+      <li>ReactJs - Admin Dashboard</li>
+      <li>ExpressJs</li>
+      <li>MongoDB</li>
+    </ol>
+    
+    <h3>Extra Dependencies:</h3>
+    
+    <ol>
+      <li>React Native
+      <ul>
+        <li>Expo</li>
+        <li><strong><a href="https://github.com/gorhom/react-native-bottom-sheet" target="_blank"><span style="color:#077052">Gorham bottom-sheet</span></a></strong></li>
+        <li>React Navigation</li>
+        <li>Axios</li>
+        <li>Tailwind CSS</li>
+      </ul>
+      </li>
+      <li>NodeJs
+      <ul>
+        <li>Bcrypt</li>
+        <li>JsonWebToken</li>
+        <li>NodeMailer</li>
+      </ul>
+      </li>
+    </ol>
+    
+    <h2>Team Members:</h2>
+    
+    <ol>
+      <li><strong><a href="https://www.linkedin.com/in/siddhi-mahadik-470092212/" target="_blank"><span style="color:#077052">Siddhi Mahadik</span></a></strong></li>
+      <li><strong><a href="https://www.linkedin.com/in/nisha-patil-44b645214/" target="_blank"><span style="color:#077052">Nisha Patil</span></a></strong></li>
+    </ol>
+    `,
+  },
+  {
+    key: 2,
+    projectName: `Health Aura - Complete Healthcare System`,
+    projectType: `Full stack`,
+    projectStatus: "Completed",
+    githubRepo:
+      "https://github.com/rahulnikam2002/Health-Aura---Your-Health-With-Us",
+    projectImage: healthAuraImage,
+    projectLiveLink: "",
+    shortDescription: `Users can find nearest Hospitals, Medicals, Doctors, Technologies used in this project are NodeJS, Express, MySQL...`,
+    projectDescription: `
 <p>Users can find nearest Hospitals, Medicals, Doctors and they can contact any doctor easily, we included functionality such as user authentication, in this I have used Bcrypt for hashing confidential data of users, I have also added change password functionality where a 4 digit OTP will be sent to the user and also it will get stored in Database in an hashed format for security purpose.</p>
 <p><br></p>
 <h2>Technologies used</h2>
@@ -39,17 +95,17 @@ export const projectData = [
     <li>User profile page (Editable).</li>
     <li>Advance search functionality (Searching by using user&apos;s Location GPS)</li>
 </ol>`,
-    },
-    {
-      key: 2,
-      projectName: `Vaccines Near Me - Frontend Project`,
-      projectType: `Frontend`,
-      projectStatus: 'Deployed',
-      projectImage: vaccinesNearMeImage,
-      githubRepo: 'https://github.com/rahulnikam2002/Covid-Vaccines-Locator',
-      projectLiveLink: 'https://vaccinesnearme.web.app',
-      shortDescription: `Its a web app which shows vaccination camp held by Government of India, We have used HTML, CSS, JavaScript...`,
-      projectDescription: `
+  },
+  {
+    key: 3,
+    projectName: `Vaccines Near Me - Frontend Project`,
+    projectType: `Frontend`,
+    projectStatus: "Deployed",
+    projectImage: vaccinesNearMeImage,
+    githubRepo: "https://github.com/rahulnikam2002/Covid-Vaccines-Locator",
+    projectLiveLink: "https://vaccinesnearme.web.app",
+    shortDescription: `Its a web app which shows vaccination camp held by Government of India, We have used HTML, CSS, JavaScript...`,
+    projectDescription: `
       <p>Vaccines Near Me is a web app which shows information related to Vaccination camp held by the Government of India, Its single page application so its very easy to navigate. and if you have a question in your mind that why we have created this, there area also apps like CoWin they too provides same information!! Yes these websites too provide same content but the difference is to see information on websites like CoWin user needs to register and this process is so lengthy, People who live in Rural area might be uncomfortable for using these websites to we have created a single page application which is very easy to use. &nbsp;</p>
       <p><br></p>
       <h2>Technologies used</h2>
@@ -60,17 +116,17 @@ export const projectData = [
           <li>CoWin API.</li>
           <li>Firebase for Deployment.</li>
       </ol>`,
-    },
-    {
-      key: 3,
-      projectName: `Prompt Engineers - Full Stack Project`,
-      projectType: `Full Stack`,
-      projectStatus: 'Deployed',
-      githubRepo: '',
-      projectImage: promptEngineersImage,
-      projectLiveLink: 'https://promptengineers.in',
-      shortDescription: `Its a project for one of the company Prompt Engineers in Pune, Technologies used are NodeJS, Express, MySQL...`,
-      projectDescription: `
+  },
+  {
+    key: 4,
+    projectName: `Prompt Engineers - Full Stack Project`,
+    projectType: `Full Stack`,
+    projectStatus: "Deployed",
+    githubRepo: "",
+    projectImage: promptEngineersImage,
+    projectLiveLink: "https://promptengineers.in",
+    shortDescription: `Its a project for one of the company Prompt Engineers in Pune, Technologies used are NodeJS, Express, MySQL...`,
+    projectDescription: `
       <p>I had developed and deployed a website for a company based in Pune named Prompt Engineers, Its a simple full stack website where I&apos;m showing all the services provided by the company. I have used NodeJS for backend, Express for server, MySQL for Database, and also created CMS for adding services, and to check contacts. This website is deployed on Apache Server (cPanel), as it was companies recommendation to host it on cPanel.</p>
       <p><br></p>
       <h2>Technologies used</h2>
@@ -81,6 +137,5 @@ export const projectData = [
           <li>HTML &amp; CSS for Structure and Designing</li>
           <li>Bootstrap.</li>
       </ol>`,
-    },
-    
-  ];
+  },
+];
